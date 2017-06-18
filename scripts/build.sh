b2f1b9d98c8b2ccdcc02ecd712b984db6b8618ae
Anubhav Mishra <anubhav.mishra@hootsuite.com>
2017-06-18 16:48:32 -0700

    removing a redundant step merging into one and some notes

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,3 +11,5 @@ run_unit_test
 # Build packages to make sure they can be compiled
 echo "Running 'make build'"
 make build-service
+
+# TODO: add parallel builds for every make target that has anything to do with testing.
