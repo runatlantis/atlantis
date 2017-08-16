d1b414fff0eefc0789c6672f68ae92b236de6fe6
Anubhav Mishra <anubhav.mishra@hootsuite.com>
2017-08-16 13:35:46 -0700

    now copying atlantis binary instead of moving it

--- scripts/e2e-deps.sh
+++ scripts/e2e-deps.sh
@@ -5,7 +5,7 @@ if [ ! -f atlantis ]; then
     echo "atlantis binary not found. exiting...."
     exit 1
 fi
-mv atlantis ${CIRCLE_WORKING_DIRECTORY}/e2e/
+cp atlantis ${CIRCLE_WORKING_DIRECTORY}/e2e/
 
 # cd into e2e folder
 cd e2e/
