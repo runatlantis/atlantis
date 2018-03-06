21cf7986265e83f33f6fd8529d16cad37ae51aa2
Luke Kysow <lkysow@gmail.com>
2018-03-06 15:26:11 -0800

    Add Mailgun DNS records for emails

--- website/terraform/modules/cloudfront_distribution/outputs.tf
+++ website/terraform/modules/cloudfront_distribution/outputs.tf
@@ -1,9 +1,9 @@
 output "domain_name" {
   description = "The domain name for this distribution. Point your Route53 records here."
-  value = "${aws_cloudfront_distribution.website_distribution.domain_name}"
+  value       = "${aws_cloudfront_distribution.website_distribution.domain_name}"
 }
 
 output "hosted_zone_id" {
   description = "Zone ID of the CloudFront distribution. Use this for your Route53 record."
-  value = "${aws_cloudfront_distribution.website_distribution.hosted_zone_id}"
+  value       = "${aws_cloudfront_distribution.website_distribution.hosted_zone_id}"
 }
