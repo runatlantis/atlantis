8aca81966f8a688dc5101beb0e149ec67d054497
Luke Kysow <lkysow@gmail.com>
2018-03-15 17:14:14 -0700

    Add Apache 2 license and copyright.
    Add Apache 2 license and Hootsuite copyright to all golang
    files. Add a disclaimer that the files have been modified
    hereafter by contributors to this repo in order to abide
    by the Apache 2 license requirements.

--- testing/assertions.go
+++ testing/assertions.go
@@ -1,6 +1,17 @@
+// Copyright 2017 HootSuite Media Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// Modified hereafter by contributors to runatlantis/atlantis.
+//
 package testing
-
-// taken from https://github.com/benbjohnson/testing
 
 import (
 	"fmt"
@@ -11,6 +22,7 @@ import (
 )
 
 // Assert fails the test if the condition is false.
+// Taken from https://github.com/benbjohnson/testing.
 func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	if !condition {
 		_, file, line, _ := runtime.Caller(1)
@@ -20,6 +32,7 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 }
 
 // Ok fails the test if an err is not nil.
+// Taken from https://github.com/benbjohnson/testing.
 func Ok(tb testing.TB, err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
@@ -29,6 +42,7 @@ func Ok(tb testing.TB, err error) {
 }
 
 // Equals fails the test if exp is not equal to act.
+// Taken from https://github.com/benbjohnson/testing.
 func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
 		_, file, line, _ := runtime.Caller(1)
