951c9a1acf63892f5e1c99db488e34090192ee35
Jay Wallace <jay.wallace@doordash.com>
2018-08-17 17:28:35 -0700

    expose repo name, owner, and branch to run step runner for custom steps

--- server/events/runtime/run_step_runner.go
+++ server/events/runtime/run_step_runner.go
@@ -34,6 +34,9 @@ func (r *RunStepRunner) Run(ctx models.ProjectCommandContext, command []string, 
 		fmt.Sprintf("ATLANTIS_TERRAFORM_VERSION=%s", tfVersion),
 		fmt.Sprintf("DIR=%s", path),
 		fmt.Sprintf("PLANFILE=%s", filepath.Join(path, GetPlanFilename(ctx.Workspace, ctx.ProjectConfig))),
+		fmt.Sprintf("REPO_OWNER=%s", ctx.BaseRepo.Owner),
+		fmt.Sprintf("REPO_NAME=%s", ctx.BaseRepo.Name),
+		fmt.Sprintf("BRANCH_NAME=%s", ctx.Pull.Branch)
 	}
 	finalEnvVars := append(baseEnvVars, customEnvVars...)
 	cmd.Env = finalEnvVars
