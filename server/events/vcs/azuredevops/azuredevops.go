ce380ec27b4edd6291cb857a0a2a080020ae7d94
David McPike <7037366+mcdafydd@users.noreply.github.com>
2019-04-02 22:00:13 -0500

    initial azuredevops client - copy from bitbucketcloud

---
+++ server/events/vcs/azuredevops/azuredevops.go
@@ -0,0 +1,6 @@
+// Package bitbucketcloud holds code for Bitbucket Cloud aka (bitbucket.org).
+// It is separate from bitbucketserver because Bitbucket Server has different
+// APIs.
+package azuredevops
+
+const BaseURL = "https://dev.azure.com"
---
+++ server/events/vcs/azuredevops/client_test.go
@@ -0,0 +1,214 @@
+package azuredevops_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+
+	"github.com/runatlantis/atlantis/server/events/models"
+	. "github.com/runatlantis/atlantis/testing"
+)
+
+// Should follow pagination properly.
+func TestClient_GetModifiedFilesPagination(t *testing.T) {
+	respTemplate := `
+{
+    "pagelen": 1,
+    "values": [
+        {
+            "type": "diffstat",
+            "status": "modified",
+            "lines_removed": 1,
+            "lines_added": 2,
+            "old": {
+                "path": "%s",
+                "type": "commit_file",
+                "links": {
+                    "self": {
+                        "href": "https://dev.azure.com/2.0/repositories/bitbucket/geordi/src/e1749643d655d7c7014001a6c0f58abaf42ad850/setup.py"
+                    }
+                }
+            },
+            "new": {
+                "path": "%s",
+                "type": "commit_file",
+                "links": {
+                    "self": {
+                        "href": "https://dev.azure.com/2.0/repositories/bitbucket/geordi/src/d222fa235229c55dad20b190b0b571adf737d5a6/setup.py"
+                    }
+                }
+            }
+        }
+    ],
+    "page": 1,
+    "size": 1
+`
+	firstResp := fmt.Sprintf(respTemplate, "file1.txt", "file2.txt")
+	secondResp := fmt.Sprintf(respTemplate, "file2.txt", "file3.txt")
+	var serverURL string
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		// The first request should hit this URL.
+		case "/2.0/repositories/owner/repo/pullrequests/1/diffstat":
+			resp := firstResp + fmt.Sprintf(`,"next": "%s/2.0/repositories/owner/repo/pullrequests/1/diffstat?page=2"}`, serverURL)
+			w.Write([]byte(resp)) // nolint: errcheck
+			return
+			// The second should hit this URL.
+		case "/2.0/repositories/owner/repo/pullrequests/1/diffstat?page=2":
+			w.Write([]byte(secondResp + "}")) // nolint: errcheck
+		default:
+			t.Errorf("got unexpected request at %q", r.RequestURI)
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+	}))
+	defer testServer.Close()
+
+	serverURL = testServer.URL
+	client := bitbucketcloud.NewClient(http.DefaultClient, "user", "pass", "runatlantis.io")
+	client.BaseURL = testServer.URL
+
+	files, err := client.GetModifiedFiles(models.Repo{
+		FullName:          "owner/repo",
+		Owner:             "owner",
+		Name:              "repo",
+		CloneURL:          "",
+		SanitizedCloneURL: "",
+		VCSHost: models.VCSHost{
+			Type:     models.BitbucketCloud,
+			Hostname: "dev.azure.com",
+		},
+	}, models.PullRequest{
+		Num: 1,
+	})
+	Ok(t, err)
+	Equals(t, []string{"file1.txt", "file2.txt", "file3.txt"}, files)
+}
+
+// If the "old" key in the list of files is nil we shouldn't error.
+func TestClient_GetModifiedFilesOldNil(t *testing.T) {
+	resp := `
+{
+  "pagelen": 500,
+  "values": [
+    {
+      "status": "added",
+      "old": null,
+      "lines_removed": 0,
+      "lines_added": 2,
+      "new": {
+        "path": "parent/child/file1.txt",
+        "type": "commit_file",
+        "links": {
+          "self": {
+            "href": "https://dev.azure.com/2.0/repositories/lkysow/atlantis-example/src/1ed8205eec00dab4f1c0a8c486a4492c98c51f8e/main.tf"
+          }
+        }
+      },
+      "type": "diffstat"
+    }
+  ],
+  "page": 1,
+  "size": 1
+}`
+
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.RequestURI {
+		// The first request should hit this URL.
+		case "/2.0/repositories/owner/repo/pullrequests/1/diffstat":
+			w.Write([]byte(resp)) // nolint: errcheck
+			return
+		default:
+			t.Errorf("got unexpected request at %q", r.RequestURI)
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+		}
+	}))
+	defer testServer.Close()
+
+	client := bitbucketcloud.NewClient(http.DefaultClient, "user", "pass", "runatlantis.io")
+	client.BaseURL = testServer.URL
+
+	files, err := client.GetModifiedFiles(models.Repo{
+		FullName:          "owner/repo",
+		Owner:             "owner",
+		Name:              "repo",
+		CloneURL:          "",
+		SanitizedCloneURL: "",
+		VCSHost: models.VCSHost{
+			Type:     models.BitbucketCloud,
+			Hostname: "dev.azure.com",
+		},
+	}, models.PullRequest{
+		Num: 1,
+	})
+	Ok(t, err)
+	Equals(t, []string{"parent/child/file1.txt"}, files)
+}
+
+func TestClient_PullIsApproved(t *testing.T) {
+	cases := []struct {
+		description string
+		testdata    string
+		exp         bool
+	}{
+		{
+			"no approvers",
+			"pull-unapproved.json",
+			false,
+		},
+		{
+			"approver is the author",
+			"pull-approved-by-author.json",
+			false,
+		},
+		{
+			"single approver",
+			"pull-approved.json",
+			true,
+		},
+		{
+			"two approvers one author",
+			"pull-approved-multiple.json",
+			true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.description, func(t *testing.T) {
+			json, err := ioutil.ReadFile(filepath.Join("testdata", c.testdata))
+			Ok(t, err)
+			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				switch r.RequestURI {
+				// The first request should hit this URL.
+				case "/2.0/repositories/owner/repo/pullrequests/1":
+					w.Write(json) // nolint: errcheck
+					return
+				default:
+					t.Errorf("got unexpected request at %q", r.RequestURI)
+					http.Error(w, "not found", http.StatusNotFound)
+					return
+				}
+			}))
+			defer testServer.Close()
+
+			client := bitbucketcloud.NewClient(http.DefaultClient, "user", "pass", "runatlantis.io")
+			client.BaseURL = testServer.URL
+
+			repo, err := models.NewRepo(models.BitbucketServer, "owner/repo", "https://dev.azure.com/owner/repo.git", "user", "token")
+			Ok(t, err)
+			approved, err := client.PullIsApproved(repo, models.PullRequest{
+				Num:        1,
+				HeadBranch: "branch",
+				Author:     "author",
+				BaseRepo:   repo,
+			})
+			Ok(t, err)
+			Equals(t, c.exp, approved)
+		})
+	}
+}
