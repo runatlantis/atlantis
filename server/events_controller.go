6e8e1c663458488128536a63c65c983f43bcab0c
PePe (Jose) Amengual <jose.amengual@gmail.com>
2020-09-24 16:41:24 -0700

    Trying to fix tests

--- server/events_controller.go
+++ server/events_controller.go
@@ -440,15 +440,17 @@ func (e *EventsController) handleCommentEvent(w http.ResponseWriter, baseRepo mo
 	}
 
 	// Check if the user who commented has the permissions to execute the 'plan' or 'apply' commands
-	ok, err := e.checkUserPermissions(baseRepo, user, parseResult.Command)
-	if err != nil {
-		e.Logger.Err("unable to comment on pull request: %s", err)
-		return
-	}
-	if !ok {
-		e.commentUserDoesNotHavePermissions(baseRepo, pullNum, user, parseResult.Command)
-		e.respond(w, logging.Warn, http.StatusForbidden, "User @%s does not have permissions to execute '%s' command", user.Username, parseResult.Command.Name.String())
-		return
+	if parseResult.Command != nil && user.Username != "" {
+		ok, err := e.checkUserPermissions(baseRepo, user, parseResult.Command)
+		if err != nil {
+			e.Logger.Err("unable to comment on pull request: %s", err)
+			return
+		}
+		if !ok {
+			e.commentUserDoesNotHavePermissions(baseRepo, pullNum, user, parseResult.Command)
+			e.respond(w, logging.Warn, http.StatusForbidden, "User @%s does not have permissions to execute '%s' command", user.Username, parseResult.Command.Name.String())
+			return
+		}
 	}
 
 	e.Logger.Debug("executing command")
