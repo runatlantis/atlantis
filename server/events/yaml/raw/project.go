beebc225bc621687772dd399cf30f19bb47adc1f
Nish Krishnan <nishkrishnan@users.noreply.github.com>
2021-08-10 10:29:52 -0700

    Add tags to project structure in atlantis.yaml. (#94)

--- server/events/yaml/raw/project.go
+++ server/events/yaml/raw/project.go
@@ -31,14 +31,15 @@ var applyRequirements = map[string]bool{
 var supportedApplyReqs = buildSupportedApplyReqs()
 
 type Project struct {
-	Name                      *string   `yaml:"name,omitempty"`
-	Dir                       *string   `yaml:"dir,omitempty"`
-	Workspace                 *string   `yaml:"workspace,omitempty"`
-	Workflow                  *string   `yaml:"workflow,omitempty"`
-	TerraformVersion          *string   `yaml:"terraform_version,omitempty"`
-	Autoplan                  *Autoplan `yaml:"autoplan,omitempty"`
-	ApplyRequirements         []string  `yaml:"apply_requirements,omitempty"`
-	DeleteSourceBranchOnMerge *bool     `yaml:"delete_source_branch_on_merge,omitempty"`
+	Name                      *string           `yaml:"name,omitempty"`
+	Dir                       *string           `yaml:"dir,omitempty"`
+	Workspace                 *string           `yaml:"workspace,omitempty"`
+	Workflow                  *string           `yaml:"workflow,omitempty"`
+	TerraformVersion          *string           `yaml:"terraform_version,omitempty"`
+	Autoplan                  *Autoplan         `yaml:"autoplan,omitempty"`
+	ApplyRequirements         []string          `yaml:"apply_requirements,omitempty"`
+	DeleteSourceBranchOnMerge *bool             `yaml:"delete_source_branch_on_merge,omitempty"`
+	Tags                      map[string]string `yaml:"tags,omitempty"`
 }
 
 func (p Project) Validate() error {
@@ -97,6 +98,7 @@ func (p Project) ToValid() valid.Project {
 	// There are no default apply requirements.
 	v.ApplyRequirements = p.ApplyRequirements
 
+	v.Tags = p.Tags
 	v.Name = p.Name
 
 	if p.DeleteSourceBranchOnMerge != nil {
