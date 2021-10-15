ed418eaae962b835616ab1fdd17fccd2e04d1b51
Daniel Grimes <dan@dcgnet.co.uk>
2021-10-15 12:41:39 +0000

    allow AzureDevOps host to be specified in the user_config

--- server/server.go
+++ server/server.go
@@ -216,8 +216,14 @@ func NewServer(userConfig UserConfig, config Config) (*Server, error) {
 	}
 	if userConfig.AzureDevopsUser != "" {
 		supportedVCSHosts = append(supportedVCSHosts, models.AzureDevops)
-		var err error
-		azuredevopsClient, err = vcs.NewAzureDevopsClient("dev.azure.com", userConfig.AzureDevopsUser, userConfig.AzureDevopsToken)
+
+		azureDevOpsHost := userConfig.AzureDevOpsHost
+		if userConfig.AzureDevOpsHost=="" {
+			azureDevOpsHost  ="dev.azure.com"
+		}
+
+		var err error		
+		azuredevopsClient, err = vcs.NewAzureDevopsClient(azureDevOpsHost, userConfig.AzureDevopsUser, userConfig.AzureDevopsToken)
 		if err != nil {
 			return nil, err
 		}
