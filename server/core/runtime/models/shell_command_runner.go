4e894c48dd9b4dd3a4f2e33cabc87df86439ab25
Aiden Scandella <git@sca.ndella.com>
2022-05-17 12:15:45 -0700

    Fix log output

--- server/core/runtime/models/shell_command_runner.go
+++ server/core/runtime/models/shell_command_runner.go
@@ -89,10 +89,10 @@ func (s *ShellCommandRunner) RunCommandAsync(ctx command.ProjectContext) (chan<-
 		stderr, _ := s.cmd.StderrPipe()
 		stdin, _ := s.cmd.StdinPipe()
 
-		ctx.Log.Debug("starting %q in %q", s.cmd, s.workingDir)
+		ctx.Log.Debug("starting %q in %q", s.command, s.workingDir)
 		err := s.cmd.Start()
 		if err != nil {
-			err = errors.Wrapf(err, "running %q in %q", s.cmd, s.workingDir)
+			err = errors.Wrapf(err, "running %q in %q", s.command, s.workingDir)
 			ctx.Log.Err(err.Error())
 			outCh <- Line{Err: err}
 			return
@@ -144,11 +144,11 @@ func (s *ShellCommandRunner) RunCommandAsync(ctx command.ProjectContext) (chan<-
 
 		// We're done now. Send an error if there was one.
 		if err != nil {
-			err = errors.Wrapf(err, "running %q in %q", s.cmd, s.workingDir)
+			err = errors.Wrapf(err, "running %q in %q", s.command, s.workingDir)
 			ctx.Log.Err(err.Error())
 			outCh <- Line{Err: err}
 		} else {
-			ctx.Log.Info("successfully ran %q in %q", s.cmd, s.workingDir)
+			ctx.Log.Info("successfully ran %q in %q", s.command, s.workingDir)
 		}
 	}()
 
