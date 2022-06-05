5a6c2cadda36cb686f61dc33e9ac789d7b054b80
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-06-05 12:53:45 -0500

    Ignore gpg signing for tests

--- server/events/working_dir.go
+++ server/events/working_dir.go
@@ -66,6 +66,8 @@ type FileWorkspace struct {
 	// from the "origin" remote. If this is false, we fetch "+refs/heads/$HEAD_BRANCH"
 	// from the "head" remote.
 	GithubAppEnabled bool
+	// use the global setting without overriding
+	GpgNoSigningEnabled bool
 }
 
 // Clone git clones headRepo, checks out the branch and then returns the absolute
@@ -240,16 +242,21 @@ func (w *FileWorkspace) forceClone(log logging.SimpleLogging,
 			{
 				"git", "fetch", fetchRemote, fetchRef,
 			},
-			// We use --no-ff because we always want there to be a merge commit.
-			// This way, our branch will look the same regardless if the merge
-			// could be fast forwarded. This is useful later when we run
-			// git rev-parse HEAD^2 to get the head commit because it will
-			// always succeed whereas without --no-ff, if the merge was fast
-			// forwarded then git rev-parse HEAD^2 would fail.
-			{
-				"git", "merge", "-q", "--no-ff", "-m", "atlantis-merge", "FETCH_HEAD",
-			},
 		}
+		if w.GpgNoSigningEnabled {
+			cmds = append(cmds, []string{
+				"git", "config", "--local", "commit.gpgsign", "false",
+			})
+		}
+		// We use --no-ff because we always want there to be a merge commit.
+		// This way, our branch will look the same regardless if the merge
+		// could be fast forwarded. This is useful later when we run
+		// git rev-parse HEAD^2 to get the head commit because it will
+		// always succeed whereas without --no-ff, if the merge was fast
+		// forwarded then git rev-parse HEAD^2 would fail.
+		cmds = append(cmds, []string{
+			"git", "merge", "-q", "--no-ff", "-m", "atlantis-merge", "FETCH_HEAD",
+		})
 	} else {
 		cmds = [][]string{
 			{
