38a3aca82d0633ca8bc31b1a4b7c36f87e4a5755
Michel Z. Santello <michelmzs@gmail.com>
2022-06-10 19:53:28 -0300

    chore: normalize tests

--- server/events/command_runner_test.go
+++ server/events/command_runner_test.go
@@ -107,6 +107,7 @@ func setup(t *testing.T) *vcsmocks.MockClient {
 
 	parallelPoolSize := 1
 	SilenceNoProjects := false
+	StatusName := "atlantis-test"
 	policyCheckCommandRunner = events.NewPolicyCheckCommandRunner(
 		dbUpdater,
 		pullUpdater,
@@ -151,6 +152,7 @@ func setup(t *testing.T) *vcsmocks.MockClient {
 		parallelPoolSize,
 		SilenceNoProjects,
 		false,
+		StatusName,
 		pullReqStatusFetcher,
 	)
 
@@ -781,7 +783,7 @@ func TestApplyMergeablityWhenPolicyCheckFails(t *testing.T) {
 		},
 	})
 
-	When(ch.VCSClient.PullIsMergeable(fixtures.GithubRepo, modelPull)).ThenReturn(true, nil)
+	When(ch.VCSClient.PullIsMergeable(fixtures.GithubRepo, modelPull, "atlantis-test")).ThenReturn(true, nil)
 
 	When(projectCommandBuilder.BuildApplyCommands(matchers.AnyPtrToEventsCommandContext(), matchers.AnyPtrToEventsCommentCommand())).Then(func(args []Param) ReturnValues {
 		return ReturnValues{
