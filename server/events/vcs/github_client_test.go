59c9fb7a0790f107da3dfa3c976f2ee69e22fdfd
Chico Venancio <francisco@argyle.com>
2022-06-10 00:07:03 -0300

    [fix] Ignore commit checks for atlantis apply on Github

--- server/events/vcs/github_client_test.go
+++ server/events/vcs/github_client_test.go
@@ -476,43 +476,75 @@ func TestGithubClient_PullIsApproved(t *testing.T) {
 
 func TestGithubClient_PullIsMergeable(t *testing.T) {
 	cases := []struct {
-		state        string
-		expMergeable bool
+		state               string
+		requiredCheckName   string
+		requiredCheckStatus string
+		expMergeable        bool
 	}{
 		{
 			"dirty",
+			"",
+			"",
 			false,
 		},
 		{
 			"unknown",
-			false,
-		},
-		{
-			"blocked",
+			"",
+			"",
 			false,
 		},
 		{
 			"behind",
+			"",
+			"",
 			false,
 		},
 		{
 			"random",
+			"",
+			"",
 			false,
 		},
 		{
 			"unstable",
+			"",
+			"",
 			true,
 		},
 		{
 			"has_hooks",
+			"",
+			"",
 			true,
 		},
 		{
 			"clean",
+			"",
+			"",
 			true,
 		},
 		{
 			"",
+			"",
+			"",
+			false,
+		},
+		{
+			"blocked",
+			"atlantis/apply",
+			"failure",
+			true,
+		},
+		{
+			"blocked",
+			"atlantis/apply",
+			"pending",
+			true,
+		},
+		{
+			"blocked",
+			"required_check",
+			"failure",
 			false,
 		},
 	}
@@ -520,21 +552,38 @@ func TestGithubClient_PullIsMergeable(t *testing.T) {
 	// Use a real GitHub json response and edit the mergeable_state field.
 	jsBytes, err := os.ReadFile("fixtures/github-pull-request.json")
 	Ok(t, err)
-	json := string(jsBytes)
+	prJson := string(jsBytes)
 
 	for _, c := range cases {
 		t.Run(c.state, func(t *testing.T) {
-			response := strings.Replace(json,
+			response := strings.Replace(prJson,
 				`"mergeable_state": "clean"`,
 				fmt.Sprintf(`"mergeable_state": "%s"`, c.state),
 				1,
 			)
+			responseStatus, _ := json.Marshal(map[string][]map[string]string{
+				"statuses": {{
+					"context": c.requiredCheckName,
+					"state": c.requiredCheckStatus,
+				},},
+			})
+			responseRequiredChecks, _ := json.Marshal(map[string][]string{
+				"contexts": {
+					c.requiredCheckName,
+				},
+			})
 
 			testServer := httptest.NewTLSServer(
 				http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 					switch r.RequestURI {
 					case "/api/v3/repos/owner/repo/pulls/1":
 						w.Write([]byte(response)) // nolint: errcheck
+						return
+					case "/api/v3/repos/owner/repo/commits/headBranch/status":
+						w.Write([]byte(responseStatus)) // nolint: errcheck
+						return
+					case "/api/v3/repos/owner/repo/branches/baseBranch/protection/required_status_checks":
+						w.Write([]byte(responseRequiredChecks)) // nolint: errcheck
 						return
 					default:
 						t.Errorf("got unexpected request at %q", r.RequestURI)
@@ -560,6 +609,8 @@ func TestGithubClient_PullIsMergeable(t *testing.T) {
 				},
 			}, models.PullRequest{
 				Num: 1,
+				HeadBranch: "headBranch",
+				BaseBranch: "baseBranch",
 			})
 			Ok(t, err)
 			Equals(t, c.expMergeable, actMergeable)
