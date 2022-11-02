a0e9134038048542ea13c18792014e67ccf4274e
Michel Z. Santello <michelmzs@gmail.com>
2022-11-02 12:36:56 -0300

    chore: update test 'TestGitlabClient_UpdateStatus'

--- server/events/vcs/gitlab_client_test.go
+++ server/events/vcs/gitlab_client_test.go
@@ -212,7 +212,7 @@ func TestGitlabClient_UpdateStatus(t *testing.T) {
 
 						body, err := io.ReadAll(r.Body)
 						Ok(t, err)
-						exp := fmt.Sprintf(`{"state":"%s","context":"src","target_url":"https://google.com","description":"description"}`, c.expState)
+						exp := fmt.Sprintf(`{"state":"%s","ref":"test","context":"src","target_url":"https://google.com","description":"description"}`, c.expState)
 						Equals(t, exp, string(body))
 						defer r.Body.Close()  // nolint: errcheck
 						w.Write([]byte("{}")) // nolint: errcheck
@@ -241,6 +241,7 @@ func TestGitlabClient_UpdateStatus(t *testing.T) {
 				Num:        1,
 				BaseRepo:   repo,
 				HeadCommit: "sha",
+				HeadBranch: "test",
 			}, c.status, "src", "description", "https://google.com")
 			Ok(t, err)
 			Assert(t, gotRequest, "expected to get the request")
