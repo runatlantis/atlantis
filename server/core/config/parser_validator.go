7c8027750d9feeaeac68b7b7eee1e8c999e49f14
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-12-20 09:49:56 -0600

    Run gofmt -s -w

--- server/core/config/parser_validator.go
+++ server/core/config/parser_validator.go
@@ -7,8 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 
-	shlex "github.com/google/shlex"
 	validation "github.com/go-ozzo/ozzo-validation"
+	shlex "github.com/google/shlex"
 	"github.com/pkg/errors"
 	"github.com/runatlantis/atlantis/server/core/config/raw"
 	"github.com/runatlantis/atlantis/server/core/config/valid"
