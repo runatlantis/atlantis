1b63a91c844a3e7c63cc42450429499fe7b77825
krrrr38 <k.kaizu38@gmail.com>
2022-12-21 13:53:42 +0900

    regenerate mock comment builder

--- server/events/mocks/mock_comment_building.go
+++ server/events/mocks/mock_comment_building.go
@@ -55,21 +55,6 @@ func (mock *MockCommentBuilder) BuildApplyComment(repoRelDir string, workspace s
 	return ret0
 }
 
-func (mock *MockCommentBuilder) BuildVersionComment(repoRelDir string, workspace string, project string) string {
-	if mock == nil {
-		panic("mock must not be nil. Use myMock := NewMockCommentBuilder().")
-	}
-	params := []pegomock.Param{repoRelDir, workspace, project}
-	result := pegomock.GetGenericMockFrom(mock).Invoke("BuildVersionComment", params, []reflect.Type{reflect.TypeOf((*string)(nil)).Elem()})
-	var ret0 string
-	if len(result) != 0 {
-		if result[0] != nil {
-			ret0 = result[0].(string)
-		}
-	}
-	return ret0
-}
-
 func (mock *MockCommentBuilder) VerifyWasCalledOnce() *VerifierMockCommentBuilder {
 	return &VerifierMockCommentBuilder{
 		mock:                   mock,
@@ -180,41 +165,6 @@ func (c *MockCommentBuilder_BuildApplyComment_OngoingVerification) GetAllCapture
 		_param3 = make([]bool, len(c.methodInvocations))
 		for u, param := range params[3] {
 			_param3[u] = param.(bool)
-		}
-	}
-	return
-}
-
-func (verifier *VerifierMockCommentBuilder) BuildVersionComment(repoRelDir string, workspace string, project string) *MockCommentBuilder_BuildVersionComment_OngoingVerification {
-	params := []pegomock.Param{repoRelDir, workspace, project}
-	methodInvocations := pegomock.GetGenericMockFrom(verifier.mock).Verify(verifier.inOrderContext, verifier.invocationCountMatcher, "BuildVersionComment", params, verifier.timeout)
-	return &MockCommentBuilder_BuildVersionComment_OngoingVerification{mock: verifier.mock, methodInvocations: methodInvocations}
-}
-
-type MockCommentBuilder_BuildVersionComment_OngoingVerification struct {
-	mock              *MockCommentBuilder
-	methodInvocations []pegomock.MethodInvocation
-}
-
-func (c *MockCommentBuilder_BuildVersionComment_OngoingVerification) GetCapturedArguments() (string, string, string) {
-	repoRelDir, workspace, project := c.GetAllCapturedArguments()
-	return repoRelDir[len(repoRelDir)-1], workspace[len(workspace)-1], project[len(project)-1]
-}
-
-func (c *MockCommentBuilder_BuildVersionComment_OngoingVerification) GetAllCapturedArguments() (_param0 []string, _param1 []string, _param2 []string) {
-	params := pegomock.GetGenericMockFrom(c.mock).GetInvocationParams(c.methodInvocations)
-	if len(params) > 0 {
-		_param0 = make([]string, len(c.methodInvocations))
-		for u, param := range params[0] {
-			_param0[u] = param.(string)
-		}
-		_param1 = make([]string, len(c.methodInvocations))
-		for u, param := range params[1] {
-			_param1[u] = param.(string)
-		}
-		_param2 = make([]string, len(c.methodInvocations))
-		for u, param := range params[2] {
-			_param2[u] = param.(string)
 		}
 	}
 	return
