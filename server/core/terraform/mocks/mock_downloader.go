eca3b536aa83a887848a7101eea6e7b5371e6e00
Ken Kaizu <k.kaizu38@gmail.com>
2022-12-23 23:25:25 +0900

    upgrade github.com/hashicorp/go-getter to v2 (#2865)

--- server/core/terraform/mocks/mock_downloader.go
+++ server/core/terraform/mocks/mock_downloader.go
@@ -4,11 +4,9 @@
 package mocks
 
 import (
+	pegomock "github.com/petergtz/pegomock"
 	"reflect"
 	"time"
-
-	go_getter "github.com/hashicorp/go-getter"
-	pegomock "github.com/petergtz/pegomock"
 )
 
 type MockDownloader struct {
@@ -26,14 +24,11 @@ func NewMockDownloader(options ...pegomock.Option) *MockDownloader {
 func (mock *MockDownloader) SetFailHandler(fh pegomock.FailHandler) { mock.fail = fh }
 func (mock *MockDownloader) FailHandler() pegomock.FailHandler      { return mock.fail }
 
-func (mock *MockDownloader) GetFile(dst string, src string, opts ...go_getter.ClientOption) error {
+func (mock *MockDownloader) GetFile(dst string, src string) error {
 	if mock == nil {
 		panic("mock must not be nil. Use myMock := NewMockDownloader().")
 	}
 	params := []pegomock.Param{dst, src}
-	for _, param := range opts {
-		params = append(params, param)
-	}
 	result := pegomock.GetGenericMockFrom(mock).Invoke("GetFile", params, []reflect.Type{reflect.TypeOf((*error)(nil)).Elem()})
 	var ret0 error
 	if len(result) != 0 {
@@ -44,14 +39,11 @@ func (mock *MockDownloader) GetFile(dst string, src string, opts ...go_getter.Cl
 	return ret0
 }
 
-func (mock *MockDownloader) GetAny(dst string, src string, opts ...go_getter.ClientOption) error {
+func (mock *MockDownloader) GetAny(dst string, src string) error {
 	if mock == nil {
 		panic("mock must not be nil. Use myMock := NewMockDownloader().")
 	}
 	params := []pegomock.Param{dst, src}
-	for _, param := range opts {
-		params = append(params, param)
-	}
 	result := pegomock.GetGenericMockFrom(mock).Invoke("GetAny", params, []reflect.Type{reflect.TypeOf((*error)(nil)).Elem()})
 	var ret0 error
 	if len(result) != 0 {
@@ -99,11 +91,8 @@ type VerifierMockDownloader struct {
 	timeout                time.Duration
 }
 
-func (verifier *VerifierMockDownloader) GetFile(dst string, src string, opts ...go_getter.ClientOption) *MockDownloader_GetFile_OngoingVerification {
+func (verifier *VerifierMockDownloader) GetFile(dst string, src string) *MockDownloader_GetFile_OngoingVerification {
 	params := []pegomock.Param{dst, src}
-	for _, param := range opts {
-		params = append(params, param)
-	}
 	methodInvocations := pegomock.GetGenericMockFrom(verifier.mock).Verify(verifier.inOrderContext, verifier.invocationCountMatcher, "GetFile", params, verifier.timeout)
 	return &MockDownloader_GetFile_OngoingVerification{mock: verifier.mock, methodInvocations: methodInvocations}
 }
@@ -113,12 +102,12 @@ type MockDownloader_GetFile_OngoingVerification struct {
 	methodInvocations []pegomock.MethodInvocation
 }
 
-func (c *MockDownloader_GetFile_OngoingVerification) GetCapturedArguments() (string, string, []go_getter.ClientOption) {
-	dst, src, opts := c.GetAllCapturedArguments()
-	return dst[len(dst)-1], src[len(src)-1], opts[len(opts)-1]
+func (c *MockDownloader_GetFile_OngoingVerification) GetCapturedArguments() (string, string) {
+	dst, src := c.GetAllCapturedArguments()
+	return dst[len(dst)-1], src[len(src)-1]
 }
 
-func (c *MockDownloader_GetFile_OngoingVerification) GetAllCapturedArguments() (_param0 []string, _param1 []string, _param2 [][]go_getter.ClientOption) {
+func (c *MockDownloader_GetFile_OngoingVerification) GetAllCapturedArguments() (_param0 []string, _param1 []string) {
 	params := pegomock.GetGenericMockFrom(c.mock).GetInvocationParams(c.methodInvocations)
 	if len(params) > 0 {
 		_param0 = make([]string, len(c.methodInvocations))
@@ -129,24 +118,12 @@ func (c *MockDownloader_GetFile_OngoingVerification) GetAllCapturedArguments() (
 		for u, param := range params[1] {
 			_param1[u] = param.(string)
 		}
-		_param2 = make([][]go_getter.ClientOption, len(c.methodInvocations))
-		for u := 0; u < len(c.methodInvocations); u++ {
-			_param2[u] = make([]go_getter.ClientOption, len(params)-2)
-			for x := 2; x < len(params); x++ {
-				if params[x][u] != nil {
-					_param2[u][x-2] = params[x][u].(go_getter.ClientOption)
-				}
-			}
-		}
 	}
 	return
 }
 
-func (verifier *VerifierMockDownloader) GetAny(dst string, src string, opts ...go_getter.ClientOption) *MockDownloader_GetAny_OngoingVerification {
+func (verifier *VerifierMockDownloader) GetAny(dst string, src string) *MockDownloader_GetAny_OngoingVerification {
 	params := []pegomock.Param{dst, src}
-	for _, param := range opts {
-		params = append(params, param)
-	}
 	methodInvocations := pegomock.GetGenericMockFrom(verifier.mock).Verify(verifier.inOrderContext, verifier.invocationCountMatcher, "GetAny", params, verifier.timeout)
 	return &MockDownloader_GetAny_OngoingVerification{mock: verifier.mock, methodInvocations: methodInvocations}
 }
@@ -156,12 +133,12 @@ type MockDownloader_GetAny_OngoingVerification struct {
 	methodInvocations []pegomock.MethodInvocation
 }
 
-func (c *MockDownloader_GetAny_OngoingVerification) GetCapturedArguments() (string, string, []go_getter.ClientOption) {
-	dst, src, opts := c.GetAllCapturedArguments()
-	return dst[len(dst)-1], src[len(src)-1], opts[len(opts)-1]
+func (c *MockDownloader_GetAny_OngoingVerification) GetCapturedArguments() (string, string) {
+	dst, src := c.GetAllCapturedArguments()
+	return dst[len(dst)-1], src[len(src)-1]
 }
 
-func (c *MockDownloader_GetAny_OngoingVerification) GetAllCapturedArguments() (_param0 []string, _param1 []string, _param2 [][]go_getter.ClientOption) {
+func (c *MockDownloader_GetAny_OngoingVerification) GetAllCapturedArguments() (_param0 []string, _param1 []string) {
 	params := pegomock.GetGenericMockFrom(c.mock).GetInvocationParams(c.methodInvocations)
 	if len(params) > 0 {
 		_param0 = make([]string, len(c.methodInvocations))
@@ -171,15 +148,6 @@ func (c *MockDownloader_GetAny_OngoingVerification) GetAllCapturedArguments() (_
 		_param1 = make([]string, len(c.methodInvocations))
 		for u, param := range params[1] {
 			_param1[u] = param.(string)
-		}
-		_param2 = make([][]go_getter.ClientOption, len(c.methodInvocations))
-		for u := 0; u < len(c.methodInvocations); u++ {
-			_param2[u] = make([]go_getter.ClientOption, len(params)-2)
-			for x := 2; x < len(params); x++ {
-				if params[x][u] != nil {
-					_param2[u][x-2] = params[x][u].(go_getter.ClientOption)
-				}
-			}
 		}
 	}
 	return
