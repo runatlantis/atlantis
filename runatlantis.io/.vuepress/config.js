850a3687600953450bceefd56e4a7b8edb19ece2
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-01-19 10:25:40 -0600

    add algolia appId

--- runatlantis.io/.vuepress/config.js
+++ runatlantis.io/.vuepress/config.js
@@ -33,6 +33,7 @@ export default defineUserConfig({
             // looking at the browser dev tools since the key is used in the API requests.
             apiKey: '3b733dff1539ca3a210775860301fa86',
             indexName: 'runatlantis',
+            appId: 'BH4D9OD16A',
             locales: {
                 '/': {
                     placeholder: 'Search Documentation',
