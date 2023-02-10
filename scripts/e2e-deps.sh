45f3a382dc45b66cfa383cfbecadf22c4799cd35
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-02-09 23:22:40 -0600

    Update e2e-deps.sh

--- scripts/e2e-deps.sh
+++ scripts/e2e-deps.sh
@@ -1,4 +1,4 @@
-GITHUB_WORKSPACE#!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # Exit immediately if a command returns a non-zero code
 set -e
@@ -9,20 +9,24 @@ if [ ! -f atlantis ]; then
     exit 1
 fi
 cp atlantis ${GITHUB_WORKSPACE}/e2e/
-echo "::add-path::${GITHUB_WORKSPACE}/e2e"
+echo "${GITHUB_WORKSPACE}/e2e" >> $GITHUB_PATH
 
 # cd into e2e folder
 cd e2e/
+
 # Download terraform
 curl -LOk https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 chmod +x terraform
-cp terraform /home/circleci/go/bin
+
 # Download ngrok to create a tunnel to expose atlantis server
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip 
-chmod +x ngrok 
+chmod +x ngrok
+
+# Download jq
 wget https://stedolan.github.io/jq/download/linux64/jq
 chmod +x jq
+
 # Copy github config file
 cp .gitconfig ~/.gitconfig
