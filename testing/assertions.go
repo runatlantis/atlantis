62f5cc28aa72f6884a622a3f4a020d84828b981a
nitrocode <7775707+nitrocode@users.noreply.github.com>
2023-02-22 00:25:05 -0600

    Update assertions.go

--- testing/assertions.go
+++ testing/assertions.go
@@ -95,19 +95,3 @@ func errLog(tb testing.TB, fmt string, args ...interface{}) {
 	tb.Helper()
 	tb.Logf("\033[31m"+fmt+"\033[39m", args...)
 }
-
-func runCmdErrCode(t *testing.T, dir string, errCode int, name string, args ...string) string {
-	t.Helper()
-	cpCmd := exec.Command(name, args...)
-	cpCmd.Dir = dir
-	cpOut, err := cpCmd.CombinedOutput()
-	cmd := strings.Join(append([]string{name}, args...), " ")
-	if err != nil {
-		if eerr, ok := err.(*exec.ExitError); ok {
-			Assert(t, errCode == eerr.ExitCode(), "unexpected exit code: want %v, got %v, running %q: %s", errCode, eerr.ExitCode(), cmd, cpCmd)
-			return string(cpOut)
-		}
-	}
-	Assert(t, false, "invalid exit code, running %q: %s", cmd, cpOut)
-	return string(cpOut)
-}
