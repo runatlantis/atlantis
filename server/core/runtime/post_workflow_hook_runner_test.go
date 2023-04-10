0b5603fc26fc16029f1de1a04e405652583d0540
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2023-04-10 14:59:50 +0900

    feat: add PULL_URL to environment variables
    Background:
    
    It is convenient to have the PR URL available so that we can link the PR
    that triggers the infrastructure changes from the audit logging system.
    For example, the google cloud provider adds user-supplied justification
    into google cloud audit logging via `CLOUDSDK_CORE_REQUEST_REASON`
    environment variable.
    
    https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference#request_reason

--- server/core/runtime/post_workflow_hook_runner_test.go
+++ server/core/runtime/post_workflow_hook_runner_test.go
@@ -51,8 +51,8 @@ func TestPostWorkflowHookRunner_Run(t *testing.T) {
 			ExpErr:  "exit status 127: running \"lkjlkj\" in",
 		},
 		{
-			Command: "echo base_repo_name=$BASE_REPO_NAME base_repo_owner=$BASE_REPO_OWNER head_repo_name=$HEAD_REPO_NAME head_repo_owner=$HEAD_REPO_OWNER head_branch_name=$HEAD_BRANCH_NAME head_commit=$HEAD_COMMIT base_branch_name=$BASE_BRANCH_NAME pull_num=$PULL_NUM pull_author=$PULL_AUTHOR",
-			ExpOut:  "base_repo_name=basename base_repo_owner=baseowner head_repo_name=headname head_repo_owner=headowner head_branch_name=add-feat head_commit=12345abcdef base_branch_name=main pull_num=2 pull_author=acme\n",
+			Command: "echo base_repo_name=$BASE_REPO_NAME base_repo_owner=$BASE_REPO_OWNER head_repo_name=$HEAD_REPO_NAME head_repo_owner=$HEAD_REPO_OWNER head_branch_name=$HEAD_BRANCH_NAME head_commit=$HEAD_COMMIT base_branch_name=$BASE_BRANCH_NAME pull_num=$PULL_NUM pull_url=$PULL_URL pull_author=$PULL_AUTHOR",
+			ExpOut:  "base_repo_name=basename base_repo_owner=baseowner head_repo_name=headname head_repo_owner=headowner head_branch_name=add-feat head_commit=12345abcdef base_branch_name=main pull_num=2 pull_url=https://github.com/runatlantis/atlantis/pull/2 pull_author=acme\n",
 		},
 		{
 			Command: "echo user_name=$USER_NAME",
@@ -92,6 +92,7 @@ func TestPostWorkflowHookRunner_Run(t *testing.T) {
 				},
 				Pull: models.PullRequest{
 					Num:        2,
+					URL:        "https://github.com/runatlantis/atlantis/pull/2",
 					HeadBranch: "add-feat",
 					HeadCommit: "12345abcdef",
 					BaseBranch: "main",
