601baf3298ebb717d72b13131f94142c3b876cef
krrrr38 <k.kaizu38@gmail.com>
2023-06-18 11:12:51 +0900

    remove pegomock generate m option, which is not support after v4

--- server/events/project_locker.go
+++ server/events/project_locker.go
@@ -22,7 +22,7 @@ import (
 	"github.com/runatlantis/atlantis/server/logging"
 )
 
-//go:generate pegomock generate -m --package mocks -o mocks/mock_project_lock.go ProjectLocker
+//go:generate pegomock generate --package mocks -o mocks/mock_project_lock.go ProjectLocker
 
 // ProjectLocker locks this project against other plans being run until this
 // project is unlocked.
