eeff28ced696934117e65213fcdfb449ea7016a3
Leandro López (inkel) <leandro.lopez@grafana.com>
2023-06-21 15:08:03 -0300

    Make code more Go-idiomatic
    While at it makes it more readable.
    
    Signed-off-by: Leandro López (inkel) <leandro.lopez@grafana.com>

--- server/core/runtime/multienv_step_runner.go
+++ server/core/runtime/multienv_step_runner.go
@@ -17,27 +17,29 @@ type MultiEnvStepRunner struct {
 // The command must return a json string containing the array of name-value pairs that are being added as extra environment variables
 func (r *MultiEnvStepRunner) Run(ctx command.ProjectContext, command string, path string, envs map[string]string) (string, error) {
 	res, err := r.RunStepRunner.Run(ctx, command, path, envs, false, valid.PostProcessRunOutputShow)
-	if err == nil {
-		if len(res) > 0 {
-			var sb strings.Builder
-			sb.WriteString("Dynamic environment variables added:\n")
+	if err != nil {
+		return "", err
+	}
 
-			envVars := strings.Split(res, ",")
-			for _, item := range envVars {
-				// Only split after the first = found in case the environment variable value has
-				// = in it (as might be the case with access tokens)
-				nameValue := strings.SplitN(strings.TrimRight(item, "\n"), "=", 2)
-				if len(nameValue) == 2 {
-					envs[nameValue[0]] = nameValue[1]
-					sb.WriteString(nameValue[0])
-					sb.WriteString("\n")
-				} else {
-					return "", fmt.Errorf("Invalid environment variable definition: %s", item)
-				}
-			}
-			return sb.String(), nil
-		}
+	if len(res) == 0 {
 		return "No dynamic environment variable added", nil
 	}
-	return "", err
+
+	var sb strings.Builder
+	sb.WriteString("Dynamic environment variables added:\n")
+
+	envVars := strings.Split(res, ",")
+	for _, item := range envVars {
+		// Only split after the first = found in case the environment variable value has
+		// = in it (as might be the case with access tokens)
+		nameValue := strings.SplitN(strings.TrimRight(item, "\n"), "=", 2)
+		if len(nameValue) == 2 {
+			envs[nameValue[0]] = nameValue[1]
+			sb.WriteString(nameValue[0])
+			sb.WriteString("\n")
+		} else {
+			return "", fmt.Errorf("Invalid environment variable definition: %s", item)
+		}
+	}
+	return sb.String(), nil
 }
