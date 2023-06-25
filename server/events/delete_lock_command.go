cea86427195d25ca7f509301e9b552ba7b0a49c0
X-Guardian <simonheather99@gmail.com>
2023-06-25 14:24:51 +0100

    Fix Delete Lock Command

--- server/events/delete_lock_command.go
+++ server/events/delete_lock_command.go
@@ -1,7 +1,11 @@
 package events
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/runatlantis/atlantis/server/core/locking"
+	"github.com/runatlantis/atlantis/server/core/runtime"
 	"github.com/runatlantis/atlantis/server/events/models"
 	"github.com/runatlantis/atlantis/server/logging"
 )
@@ -32,8 +36,20 @@ func (l *DefaultDeleteLockCommand) DeleteLock(id string) (*models.ProjectLock, e
 	if lock == nil {
 		return nil, nil
 	}
+	repoDir, err := l.WorkingDir.GetWorkingDir(lock.Pull.BaseRepo, lock.Pull, lock.Workspace)
+	if err != nil {
+		return nil, err
+	}
 
-	l.deleteWorkingDir(*lock)
+	// The locks controller currently has no implementation of Atlantis project names, so this
+	// is hardcoded to an empty string.
+	projectName := ""
+	planPath := filepath.Join(repoDir, lock.Project.Path, runtime.GetPlanFilename(lock.Workspace, projectName))
+	l.Logger.Info("Deleting plan: " + planPath)
+
+	if removeErr := os.Remove(planPath); removeErr != nil {
+		l.Logger.Warn("Failed to delete plan: %s", removeErr)
+	}
 	return lock, nil
 }
 
