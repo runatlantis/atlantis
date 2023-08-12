13186bccbd444a8a15b07af033672b767abedfba
Alexandru Marcencov <marchenkov.alexander@gmail.com>
2023-08-12 12:27:40 +0200

    add script execution from docker-entrypoint.d folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -43,4 +43,29 @@ if [ "$(id -u)" = 0 ] && [ "$1" = 'atlantis' ]; then
     set -- gosu atlantis "$@"
 fi
 
+# If we need to install some tools at entrypoint level, we can add shell scripts
+# in folder /docker-entrypoint.d/ with extension .sh and this scripts will be executed
+# at entrypount level.
+if /usr/bin/find "/docker-entrypoint.d/" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+  echo "/docker-entrypoint.d/ is not empty, will attempt to perform script execition"
+  echo "Looking for shell scripts in /docker-entrypoint.d/"
+  find "/docker-entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
+    case "$f" in
+      *.sh)
+        if [ -x "$f" ]; then
+          echo "Launching $f";
+          "$f"
+        else
+          # warn on shell scripts without exec bit
+          echo "Ignoring $f, not executable";
+        fi
+        ;;
+      *) echo "Ignoring $f";;
+    esac
+  done
+  echo "Configuration complete; ready for start up"
+else
+  echo "No files found in /docker-entrypoint.d/, skipping"
+fi
+
 exec "$@"
