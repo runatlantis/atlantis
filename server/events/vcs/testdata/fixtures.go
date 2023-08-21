760190629e8b234b7e9b0d2ca8159d5367190092
Finn Arne Gangstad <finnag@gmail.com>
2023-08-21 17:38:17 +0200

    fix: complete the upgrade to github v54 to unbreak tests (#3691)

--- server/events/vcs/testdata/fixtures.go
+++ server/events/vcs/testdata/fixtures.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/go-github/v53/github"
+	"github.com/google/go-github/v54/github"
 	"github.com/mcdafydd/go-azuredevops/azuredevops"
 )
 
