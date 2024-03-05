cb8c2e983e96800908863d0034e4649981c74114
Fitz <a.fitzpatrick@efg.gg>
2024-03-05 12:49:35 +0000

    fix: use pipeline id for commit status

--- server/events/vcs/gitlab_client_test.go
+++ server/events/vcs/gitlab_client_test.go
@@ -303,7 +303,7 @@ func TestGitlabClient_UpdateStatus(t *testing.T) {
 
 						body, err := io.ReadAll(r.Body)
 						Ok(t, err)
-						exp := fmt.Sprintf(`{"state":"%s","ref":"patch-1-merger","context":"src","target_url":"https://google.com","description":"description"}`, c.expState)
+						exp := fmt.Sprintf(`{"state":"%s","ref":"patch-1-merger","context":"src","target_url":"https://google.com","description":"description","pipeline_id":488598}`, c.expState)
 						Equals(t, exp, string(body))
 						defer r.Body.Close()  // nolint: errcheck
 						w.Write([]byte("{}")) // nolint: errcheck
