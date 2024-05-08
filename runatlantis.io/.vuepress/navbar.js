e2d4520beef2a5d5e461fe95299e5fe7aabc6a6c
Rui Chen <rui@chenrui.dev>
2024-05-08 16:57:19 -0400

    drop vuepress files

--- runatlantis.io/.vuepress/navbar.js
+++
@@ -1,8 +0,0 @@
-const en = [
-  { text: 'Home', link: '/' },
-  { text: 'Guide', link: '/guide/' },
-  { text: 'Docs', link: '/docs/' },
-  { text: 'Blog', link: 'https://medium.com/runatlantis' },
-];
-
-export default { en };
