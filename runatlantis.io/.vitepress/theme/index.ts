f7cd2072bddeffaca1853b95ac6b6b6ae9362843
Rui Chen <rui@chenrui.dev>
2024-05-14 15:43:17 -0400

    feat: add site banner for survey
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- runatlantis.io/.vitepress/theme/index.ts
+++ runatlantis.io/.vitepress/theme/index.ts
@@ -1,5 +1,13 @@
 import DefaultTheme from "vitepress/theme";
+import { defineAsyncComponent, h } from 'vue';
 import "./index.scss";
 import "./palette.scss";
 
-export default DefaultTheme;
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'layout-top': () => h(defineAsyncComponent(() => import('../components/Banner.vue')))
+    });
+  }
+};
