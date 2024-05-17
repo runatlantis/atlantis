770526be150ca5fe6597370a8a8214929d954e32
Christian Winther <jippignu@gmail.com>
2024-05-18 00:40:36 +0200

    fix(docs): try a different title for visuals

--- runatlantis.io/.vitepress/navbars.ts
+++ runatlantis.io/.vitepress/navbars.ts
@@ -4,7 +4,7 @@ const en = [
   { text: "Docs", link: "/docs" },
   { text: "Contributing", link: "/contributing" },
   { text: "Blog", link: "/blog" },
-  { text: "Survey", link: "https://docs.google.com/forms/d/1fOGWkdinDV2_46CZvzQRdz8401ypZR8Z-iwkNNt3EX0" },
+  { text: "2024 Usage Survey", link: "https://docs.google.com/forms/d/1fOGWkdinDV2_46CZvzQRdz8401ypZR8Z-iwkNNt3EX0" },
 ];
 
 export { en };
