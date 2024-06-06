c0883ced525998153c1c242ca64d3bf7abce3e99
Benoit Toulme <benoit.toulme@gmail.com>
2024-06-06 12:11:00 -0700

    doc

--- server/events/vcs/common/common.go
+++ server/events/vcs/common/common.go
@@ -12,18 +12,21 @@ func AutomergeCommitMsg(pullNum int) string {
 	return fmt.Sprintf("[Atlantis] Automatically merging after successful apply: PR #%d", pullNum)
 }
 
-// SplitComment splits comment into a slice of comments that are under maxSize.
-// It appends sepEnd to all comments that have a following comment.
-// It prepends sepStart to all comments that have a preceding comment.
-// If maxCommentsPerCommand is non-zero, it never returns more than maxCommentsPerCommand
-// comments, and it appends truncationFooter to the final comment if it would have
-// produced more comments.
+/*
+SplitComment splits comment into a slice of comments that are under maxSize.
+- It appends sepEnd to all comments that have a following comment.
+- It prepends sepStart to all comments that have a preceding comment.
+- If maxCommentsPerCommand is non-zero, it never returns more than maxCommentsPerCommand
+comments, and it truncates the beginning of the comment to preserve the end of the comment string,
+which usually contains more important information, such as warnings, errors, and the plan summary.
+- SplitComment appends the truncationHeader to the first comment if it would have produced more comments.
+*/
 func SplitComment(comment string, maxSize int, sepEnd string, sepStart string, maxCommentsPerCommand int, truncationHeader string) []string {
 	if len(comment) <= maxSize {
 		return []string{comment}
 	}
 
-	// No comment contains both sepEnd and truncationFooter, so we only have to count their max.
+	// No comment contains both sepEnd and truncationHeader, so we only have to count their max.
 	maxWithSep := maxSize - max(len(sepEnd), len(truncationHeader)) - len(sepStart)
 	var comments []string
 	numPotentialComments := int(math.Ceil(float64(len(comment)) / float64(maxWithSep)))
