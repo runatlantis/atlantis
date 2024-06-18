f8b9b67bd6b050bb9f655db731cc987449e44356
Luke Massa <lmassa@tripadvisor.com>
2024-06-17 23:26:38 -0400

    feat: Add survey results blog post

--- runatlantis.io/.vitepress/components/Banner.vue
+++ runatlantis.io/.vitepress/components/Banner.vue
@@ -24,12 +24,7 @@ const dismiss = () => {
   <div ref="el" class="banner">
     <div class="text">
       <p>
-        <del>
-          In April 2024, the Core Atlantis Team launched an anonymous survey to better understand our community's needs and help prioritize our roadmap.
-        </del>
-      </p>
-      <p>
-        That survey is now closed, we will compile the results and publish a summary as a blog post soon!
+      In April 2024, the Core Atlantis Team launched an anonymous survey to better understand our community's needs and help prioritize our roadmap, see the results <a href="/blog/2024/april-2024-survey-results">here!</a>
       </p>
       <p style="color: #FFD700; font-weight: bold;">
         Exciting news! The Atlantis project is now in the CNCF TOC vote—show your support and cast your vote <a href="https://github.com/cncf/sandbox/issues/60#issuecomment-2161214347">here</a>.
