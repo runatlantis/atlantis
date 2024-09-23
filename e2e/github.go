f5576cc8db352c935fbc9edb4dc80b4852c9b14f
nitrocode <7775707+nitrocode@users.noreply.github.com>
2024-09-23 10:02:00 -0500

    chore: update e2e github to use HookConfig
    Signed-off-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- e2e/github.go
+++ e2e/github.go
@@ -81,13 +81,15 @@ func (g GithubClient) Clone(cloneDir string) error {
 }
 
 func (g GithubClient) CreateAtlantisWebhook(ctx context.Context, hookURL string) (int64, error) {
+	contentType := "json"
+	hookConfig := &github.HookConfig{
+		ContentType: &contentType,
+		URL: &hookURL,
+	}
 	// create atlantis hook
 	atlantisHook := &github.Hook{
 		Events: []string{"issue_comment", "pull_request", "push"},
-		Config: map[string]interface{}{
-			"url":          hookURL,
-			"content_type": "json",
-		},
+		Config: hookConfig,
 		Active: github.Bool(true),
 	}
 
