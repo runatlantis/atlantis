568f3bbfeb852d3629e58c0be5fdf72d478d8c87
bakayolo <benjamin.apprederisse@gmail.com>
2024-11-02 14:42:54 -0700

    Update cmd/server.go
    Co-authored-by: Simon Heather <32168619+X-Guardian@users.noreply.github.com>
    Signed-off-by: bakayolo <benjamin.apprederisse@gmail.com>

--- cmd/server.go
+++ cmd/server.go
@@ -443,8 +443,8 @@ var stringFlags = map[string]stringFlag{
 	},
 	IgnoreVCSStatusNames: {
 		description:  "Comma separated list of VCS status names from other atlantis services." +
-   			" When `gh-allow-mergeable-bypass-apply` is true, will ignore status checks (e.g. `status1/plan`, `status1/apply`, `status2/plan`, `status2/apply`) from other atlantis services when checking if the PR is mergeable." +
-			" Only for Github but can be extended to other VCS in the future.",
+   			" When `gh-allow-mergeable-bypass-apply` is true, will ignore status checks (e.g. `status1/plan`, `status1/apply`, `status2/plan`, `status2/apply`) from other Atlantis services when checking if the PR is mergeable." +
+			" Currently only implemented for GitHub.",
 		defaultValue: DefaultIgnoreVCSStatusNames,
 	},
 	VCSStatusName: {
