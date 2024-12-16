569f94d3164a9347a109ccaf1995844b51eb5d13
Philipp Winkler <mail@philslab.ninja>
2024-12-16 13:30:20 +0100

    set DisableCloneURLCheckFlag to false for server_test
    Signed-off-by: Philipp Winkler <mail@philslab.ninja>

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -158,7 +158,7 @@ var testFlags = map[string]interface{}{
 	EnablePolicyChecksFlag:           false,
 	EnableRegExpCmdFlag:              false,
 	EnableDiffMarkdownFormat:         false,
-	DisableCloneURLCheckFlag:         true,
+	DisableCloneURLCheckFlag:         false,
 }
 
 func TestExecute_Defaults(t *testing.T) {
