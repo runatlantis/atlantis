7aa575c6de71d623f103debfdfa2ad1f9a8ad0f7
Petr Bubenik <petr.bubenik@drmax.eu>
2025-02-16 20:42:05 +0100

    fix linter problems

--- server/events/vcs/testdata/fixtures.go
+++ server/events/vcs/testdata/fixtures.go
@@ -21,9 +21,9 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/drmaxgit/go-azuredevops/azuredevops"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/go-github/v68/github"
-	"github.com/drmaxgit/go-azuredevops/azuredevops"
 )
 
 var PullEvent = github.PullRequestEvent{
