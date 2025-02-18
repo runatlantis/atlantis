ebceed5b012c0109d4e3d10cbaca0932b28d5c39
Brett Galkowski <bgalkows@uci.edu>
2025-02-17 21:05:08 -0800

    Revert loop break test

--- server/events/project_command_runner.go
+++ server/events/project_command_runner.go
@@ -520,9 +520,6 @@ func (p *DefaultProjectCommandRunner) doPolicyCheck(ctx command.ProjectContext) 
 
 	inputPolicySets := ctx.PolicySets.PolicySets
 	for i, output := range outputs {
-		if i >= len(inputPolicySets) {
-			break
-		}
 		index = i
 		policySetName := inputPolicySets[index].Name
 		if !ctx.CustomPolicyCheck {
