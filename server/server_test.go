9eb570ec15937a3ef1ae7a7501ded0dd2702f050
X-Guardian <32168619+X-Guardian@users.noreply.github.com>
2025-03-02 17:55:36 +0000

    Remove redundant test
    Signed-off-by: X-Guardian <32168619+X-Guardian@users.noreply.github.com>

--- server/server_test.go
+++ server/server_test.go
@@ -289,12 +289,3 @@ func TestParseAtlantisURL(t *testing.T) {
 		})
 	}
 }
-
-func TestCommandRunnerVCSClientInitialized(t *testing.T) {
-	s, _ := server.NewServer(server.UserConfig{
-		AtlantisURL: "http://example.com",
-	},
-		server.Config{},
-	)
-	Assert(t, s.CommandRunner.VCSClient != nil, "VCSClient must not be nil.")
-}
