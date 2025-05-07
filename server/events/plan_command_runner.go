cb5c6830412e8a3946f400869278ee6c928c6c0d
yasinlachiny <your_email@example.com>
2025-05-07 23:03:21 +0200

    fix: do not unlock after replanning
    Signed-off-by: yasinlachiny <your-email@example.com>

--- server/events/plan_command_runner.go
+++ server/events/plan_command_runner.go
@@ -1,5 +1,7 @@
 package events
 
+import "strconv"
+
 import (
 	"github.com/runatlantis/atlantis/server/core/locking"
 	"github.com/runatlantis/atlantis/server/events/command"
@@ -239,10 +241,6 @@ func (p *PlanCommandRunner) run(ctx *command.Context, cmd *CommentCommand) {
 	if !cmd.IsForSpecificProject() {
 		ctx.Log.Debug("deleting previous plans and locks")
 		p.deletePlans(ctx)
-		_, err = p.lockingLocker.UnlockByPull(baseRepo.FullName, pull.Num)
-		if err != nil {
-			ctx.Log.Err("deleting locks: %s", err)
-		}
 	}
 
 	// Only run commands in parallel if enabled
@@ -254,6 +252,24 @@ func (p *PlanCommandRunner) run(ctx *command.Context, cmd *CommentCommand) {
 		result = runProjectCmds(projectCmds, p.prjCmdRunner.Plan)
 	}
 	ctx.CommandHasErrors = result.HasErrors()
+
+	for i, projResult := range result.ProjectResults {
+		projCtx := projectCmds[i]
+
+		if projResult.PlanStatus() == models.PlannedNoChangesPlanStatus || projResult.PlanStatus() == models.ErroredPlanStatus {
+			ctx.Log.Info("Keeping lock for project '%s' (no changes or error)", projCtx.ProjectName)
+			continue
+		}
+
+		// delete lock only if there are changes
+		ctx.Log.Info("Deleting lock for project '%s' (changes detected)", projCtx.ProjectName)
+		lockID := projCtx.BaseRepo.FullName + "/" + strconv.Itoa(projCtx.Pull.Num) + "/" + projCtx.ProjectName + "/" + projCtx.Workspace
+
+		_, err := p.lockingLocker.Unlock(lockID)
+		if err != nil {
+			ctx.Log.Err("failed unlocking project '%s': %s", projCtx.ProjectName, err)
+		}
+	}
 
 	if p.autoMerger.automergeEnabled(projectCmds) && result.HasErrors() {
 		ctx.Log.Info("deleting plans because there were errors and automerge requires all plans succeed")
