52b8dff2cdf1ede0ac32d4984f5e0b49ad87f5b2
Luke Massa <lmassa@tripadvisor.com>
2025-05-13 17:28:02 -0400

    Fix formatting
    Signed-off-by: Luke Massa <lmassa@tripadvisor.com>

--- cmd/server_test.go
+++ cmd/server_test.go
@@ -24,10 +24,10 @@ import (
 	"strings"
 	"testing"
 
+	yaml "github.com/goccy/go-yaml"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	yaml "github.com/goccy/go-yaml"
 
 	"github.com/runatlantis/atlantis/server"
 	"github.com/runatlantis/atlantis/server/events/vcs/testdata"
