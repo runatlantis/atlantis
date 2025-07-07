3ac5534dfd47532a044fd323958b933aaa0dfd8c
Cuong (Bill) M. NGO <cuong.ngominh@plenti.com.au>
2025-07-07 13:51:29 +0700

    Allow access from the plentiau-security account

--- terraform/infra/ecr.tf
+++ terraform/infra/ecr.tf
@@ -2,7 +2,10 @@ module "atlantis" {
   source  = "app.terraform.io/plentiau/ecr/aws"
   version = "1.0.1"
 
-  ecr_repository_name       = "buildtools/atlantis"
-  allowed_read_account_ids  = [data.aws_caller_identity.current.account_id]
+  ecr_repository_name = "buildtools/atlantis"
+  allowed_read_account_ids = [
+    data.aws_caller_identity.current.account_id,
+    local.plentiau_security_account_id
+  ]
   allowed_write_account_ids = [data.aws_caller_identity.current.account_id]
 }
---
+++ terraform/infra/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  plentiau_security_account_id = 162555222078
+}
