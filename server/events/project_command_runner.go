53f8694eb95c103c6ba23cef276ecf34d06a0864
PePe Amengual <2208324+jamengual@users.noreply.github.com>
2025-08-12 10:41:36 -0700

    fix: add bounds checking for custom policy sets
    When using custom policy checks, ensure we don't access inputPolicySets
    out of bounds when iterating over outputs. If there are more outputs
    than policy sets, fallback to 'Custom' as the policy set name.
    
    This prevents potential index out of range panics when the number of
    outputs exceeds the number of configured policy sets.
    
    Signed-off-by: Pepe Amengual <pepe.amengual@example.com>
    Signed-off-by: PePe Amengual <2208324+jamengual@users.noreply.github.com>
    Signed-off-by: Brett Galkowski <bgalkows@uci.edu>
    Signed-off-by: dimisjim <dimitris.moraitidis@gmail.com>

--- server/events/project_command_runner.go
+++ server/events/project_command_runner.go
@@ -530,7 +530,10 @@ func (p *DefaultProjectCommandRunner) doPolicyCheck(ctx command.ProjectContext) 
 			preConftestOutput = append(preConftestOutput, output)
 		} else {
 			// Using a policy tool other than Conftest, manually building result struct
-			policySetName := inputPolicySets[index].Name
+			policySetName := "Custom"
+			if index < len(inputPolicySets) {
+				policySetName = inputPolicySets[index].Name
+			}
 			passed := !strings.Contains(strings.ToLower(output), "fail")
 			policySetResults = append(policySetResults, models.PolicySetResult{PolicySetName: policySetName, PolicyOutput: output, Passed: passed, ReqApprovals: 1, CurApprovals: 0})
 			preConftestOutput = append(preConftestOutput, "")
