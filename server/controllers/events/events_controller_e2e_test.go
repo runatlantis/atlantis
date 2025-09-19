e282f31a550c9b5398f3e373cee90d4091945b34
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-09-19 22:09:19 +0200

    events: ensure that cancel cleans up working dir lock
    Signed-off-by: Ramon Vermeulen <ramonvermeulen98@gmail.com>

--- server/controllers/events/events_controller_e2e_test.go
+++ server/controllers/events/events_controller_e2e_test.go
@@ -1468,6 +1468,8 @@ func setupE2E(t *testing.T, repoDir string, opt setupOption) (events_controllers
 
 	Ok(t, err)
 
+	cancellationTracker := events.NewCancellationTracker()
+
 	projectCommandRunner := &events.DefaultProjectCommandRunner{
 		VcsClient:        e2eVCSClient,
 		Locker:           projectLocker,
@@ -1503,6 +1505,7 @@ func setupE2E(t *testing.T, repoDir string, opt setupOption) (events_controllers
 		CommandRequirementHandler: &events.DefaultCommandRequirementHandler{
 			WorkingDir: workingDir,
 		},
+		CancellationTracker: cancellationTracker,
 	}
 
 	dbUpdater := &events.DBUpdater{
@@ -1552,6 +1555,7 @@ func setupE2E(t *testing.T, repoDir string, opt setupOption) (events_controllers
 		e2eStatusUpdater,
 		projectCommandBuilder,
 		projectCommandRunner,
+		cancellationTracker,
 		dbUpdater,
 		pullUpdater,
 		policyCheckCommandRunner,
@@ -1571,6 +1575,7 @@ func setupE2E(t *testing.T, repoDir string, opt setupOption) (events_controllers
 		e2eStatusUpdater,
 		projectCommandBuilder,
 		projectCommandRunner,
+		cancellationTracker,
 		autoMerger,
 		pullUpdater,
 		dbUpdater,
