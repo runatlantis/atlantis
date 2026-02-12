f2332ead1758c656409df315030ea51ed8edc4e1
Gavin Williams <fatmcgav@gmail.com>
2026-02-12 09:14:29 +0000

    fix: Prevent errors when writing to broken websockets
    This commit attempts to resolve the errors identified in #6180, by
    adding a `sync.Mutex` around the websocket channels.
    
    **Disclaimer:** This code was written by Gemini based on the exceptions
    in #6180, and reviewed by myself.

--- server/controllers/websocket/writer.go
+++ server/controllers/websocket/writer.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
@@ -26,22 +27,45 @@ type Writer struct {
 
 func (w *Writer) Write(rw http.ResponseWriter, r *http.Request, input chan string) error {
 	conn, err := w.upgrader.Upgrade(rw, r, nil)
-
 	if err != nil {
 		return errors.Wrap(err, "upgrading websocket connection")
 	}
 
+	var connMu sync.Mutex
+	done := make(chan struct{})
+
+	// read from the socket to detect a disconnection
+	go func() {
+		defer close(done)
+		for {
+			if _, _, err := conn.ReadMessage(); err != nil {
+				return
+			}
+		}
+	}()
+
 	// block on reading our input channel
-	for msg := range input {
-		if err := conn.WriteMessage(websocket.BinaryMessage, []byte("\r"+msg+"\n")); err != nil {
-			w.log.Warn("Failed to write ws message: %s", err)
-			return err
+	for {
+		select {
+		case msg, ok := <-input:
+			if !ok {
+				// input channel is closed, close the connection
+				if err := conn.Close(); err != nil {
+					w.log.Warn("Failed to close ws connection: %s", err)
+				}
+				return nil
+			}
+
+			connMu.Lock()
+			err := conn.WriteMessage(websocket.BinaryMessage, []byte("\r"+msg+"\n"))
+			connMu.Unlock()
+			if err != nil {
+				w.log.Warn("Failed to write ws message: %s", err)
+				return err
+			}
+		case <-done:
+			// client disconnected
+			return nil
 		}
 	}
-
-	// close ws conn after input channel is closed
-	if err = conn.Close(); err != nil {
-		w.log.Warn("Failed to close ws connection: %s", err)
-	}
-	return nil
 }
