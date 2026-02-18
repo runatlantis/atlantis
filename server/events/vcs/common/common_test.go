54b4b72a59d8ef005c1894a852ddce9b728e2402
Ross Strickland <rkstrickland@gmail.com>
2026-02-18 11:46:25 -0700

    Fix intelligent comment splits
    Signed-off-by: Ross Strickland <rkstrickland@gmail.com>

--- server/events/vcs/common/common_test.go
+++ server/events/vcs/common/common_test.go
@@ -53,8 +53,8 @@ func TestSplitComment_DynamicSeparators(t *testing.T) {
 			},
 			common.CodeInDetails: {
 				SepEnd:           "```\n</details>\n<!-- END -->",
-				SepStart:         fmt.Sprintf("```diff\n%s", baseStart),
-				TruncationHeader: "```diff\n<!-- TRUNCATED -->",
+				SepStart:         fmt.Sprintf("<details><summary>Show Output</summary>\n\n```diff\n%s", baseStart),
+				TruncationHeader: "<details><summary>Show Output</summary>\n\n```diff\n<!-- TRUNCATED -->",
 			},
 			common.InlineCode: {
 				SepEnd:           "`\n<!-- END -->",
@@ -125,27 +125,27 @@ func TestSplitComment_DynamicSeparators(t *testing.T) {
 		},
 		{
 			name:          "NoClosure - Basic text splitting",
-			comment:       "This is a long comment that will be split. " + strings.Repeat("This is additional content to make the comment longer so it will be split. ", 5),
-			maxSize:       200,
+			comment:       "This is a long comment that will be split. This is additional content to make the comment longer so it will be split.",
+			maxSize:       70,
 			maxComments:   0,
 			command:       "plan",
 			expectedCount: 3,
 			expectedComments: []string{
-				"This is a long comment that will be split. This is additional conten<!-- END -->",
-				"<!-- START plan -->t to make the comment longer so it will be split. This is additional content to make the comment longer so it will be split. This is additional content to make the comme<!-- END -->",
-				"<!-- START plan -->nt longer so it will be split. This is additional content to make the comment longer so it will be split. This is additional content to make the comment longer so it will be split. ",
+				"This is a long comment that will <!-- END -->",
+				"<!-- START plan -->be split. This is additional content <!-- END -->",
+				"<!-- START plan -->to make the comment longer so it will be split.",
 			},
 		},
 		{
 			name:          "CodeBlock - Splitting within code block",
-			comment:       "Here's some code:\n```\nterraform plan\noutput here\n```\nAnd more text. " + strings.Repeat("This is additional content to make the comment longer so it will be split. ", 3),
-			maxSize:       200,
+			comment:       "Here's some code:\n```\nterraform plan\noutput here\n```\nAnd more text. This is additional content to make the comment longer so it will be split.",
+			maxSize:       100,
 			maxComments:   0,
 			command:       "plan",
 			expectedCount: 2,
 			expectedComments: []string{
-				"Here's some code:\n```\nterraform plan\noutput here\n```\nAnd more text. This is additional content to make the comme<!-- END -->",
-				"<!-- START plan -->nt longer so it will be split. This is additional content to make the comment longer so it will be split. This is additional content to make the comment longer so it will be split. ",
+				"Here's some code:\n```\nterraform plan\noutput here\n```\nAnd more <!-- END -->",
+				"<!-- START plan -->text. This is additional content to make the comment longer so it will be split.",
 			},
 		},
 		{
@@ -154,36 +154,175 @@ func TestSplitComment_DynamicSeparators(t *testing.T) {
 			maxSize:       200,
 			maxComments:   0,
 			command:       "plan",
-			expectedCount: 3,
+			expectedCount: 4,
 			expectedComments: []string{
-				"<details><summary>Show Output</summary>\n\nSome details content here. This is addi</details>\n<!-- END -->",
-				"<details><summary>Show Output</summary>\n<!-- START plan -->tional content to make the comment longer so it will be split. This is additional content to make the comment longer s</details>\n<!-- END -->",
-				"<!-- START plan -->o it will be split. This is additional content to make the comment longer so it will be split. This is additional content to make the comment longer so it will be split. \n</details>",
+				"<details><summary>Show Output</summary>\n\nSome details content here. This is additional content to make the comment longer so it will be </details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan -->split. This is additional content to make the comment longer so it will be split. This is additional content to make </details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan -->the comment longer so it will be split. This is additional content to make the comment longer so it will be split. \n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan --></details>",
 			},
 		},
 		{
-			name:          "CodeInDetails - Splitting within code block inside details",
-			comment:       "<details><summary>Show Output</summary>\n\n```\nterraform apply\nsome output\n```\n</details>\nMore content. " + strings.Repeat("This is additional content to make the comment longer so it will be split. ", 3),
+			name: "CodeInDetails - Splitting within code block inside details",
+			comment: "<details><summary>Show Output</summary>\n\n```diff\n" +
+				strings.Repeat("Line of terraform output\n", 20) +
+				"```\n</details>",
 			maxSize:       200,
 			maxComments:   0,
-			command:       "apply",
-			expectedCount: 2,
+			command:       "plan",
+			expectedCount: 5,
+			// Split inside code block within details; continuation reopens details + code block
 			expectedComments: []string{
-				"<details><summary>Show Output</summary>\n\n```\nterraform apply\nsome output\n```\n</details>\nMore content. This is additional content to make the commen<!-- END -->",
-				"<!-- START apply -->t longer so it will be split. This is additional content to make the comment longer so it will be split. This is additional content to make the comment longer so it will be split. ",
+				"<details><summary>Show Output</summary>\n\n```diff\nLine of terraform output\nLine of terraform output\nLine of terraform output\nLine of terraform output\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->Line of terraform output\nLine of terraform output\nLine of terraform output\nLine of terraform output\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->Line of terraform output\nLine of terraform output\nLine of terraform output\nLine of terraform output\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->Line of terraform output\nLine of terraform output\nLine of terraform output\nLine of terraform output\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->Line of terraform output\nLine of terraform output\nLine of terraform output\nLine of terraform output\n```\n</details>",
 			},
 		},
 		{
 			name:          "InlineCode - Splitting within inline code block",
-			comment:       "Here is some text with a very long inline code: `" + strings.Repeat("some_very_long_function_name_", 15) + "` and more text after.",
+			comment:       "Here is text: `" + strings.Repeat("a", 150) + "` end.",
+			maxSize:       100,
+			maxComments:   0,
+			command:       "plan",
+			expectedCount: 4,
+			expectedComments: []string{
+				"Here is text: `" + strings.Repeat("a", 18) + "`\n<!-- END -->",
+				"<!-- START plan -->`" + strings.Repeat("a", 66) + "`\n<!-- END -->",
+				"<!-- START plan -->`" + strings.Repeat("a", 66) + "` <!-- END -->",
+				"<!-- START plan -->end.",
+			},
+		},
+		{
+			name: "CodeInDetails_DeeplyNested - Splitting within nested structures inside details",
+			comment: "<details><summary>Show Output</summary>\n\n```diff\n" +
+				`  # aws_iam_role.task will be created
++   resource "aws_iam_role" "task" {
++       arn                   = (known after apply)
++       assume_role_policy    = jsonencode(
+            {
++               Statement = [
++                   {
++                       Action    = "sts:AssumeRole"
++                       Effect    = "Allow"
++                       Principal = {
++                           Service = "ecs-tasks.amazonaws.com"
+                        }
+                    },
+                ]
++               Version   = "2012-10-17"
+            }
+        )
++       create_date           = (known after apply)
+    }
+` + "\n```\n</details>",
 			maxSize:       200,
 			maxComments:   0,
 			command:       "plan",
-			expectedCount: 3,
+			expectedCount: 8,
 			expectedComments: []string{
-				"Here is some text with a very long inline code: `some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function`\n<!-- END -->",
-				"<!-- START plan -->`_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_`\n<!-- END -->",
-				"<!-- START plan -->function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_some_very_long_function_name_` and more text after.",
+				"<details><summary>Show Output</summary>\n\n```diff\n  # aws_iam_role.task will be created\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->+   resource \"aws_iam_role\" \"task\" {\n+       arn                   = (known after apply)\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->+       assume_role_policy    = jsonencode(\n            {\n+               Statement = [\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->+                   {\n+                       Action    = \"sts:AssumeRole\"\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->+                       Effect    = \"Allow\"\n+                       Principal = {\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->+                           Service = \"ecs-tasks.amazonaws.com\"\n                        }\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->                    },\n                ]\n+               Version   = \"2012-10-17\"\n```\n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n\n```diff\n<!-- START plan -->            }\n        )\n+       create_date           = (known after apply)\n    }\n\n```\n</details>",
+			},
+		},
+		{
+			name:          "DetailsBlockWithAttributes - Splitting within details block with attributes",
+			comment:       "<details open><summary>Show Output</summary>\n\nSome details content here. " + strings.Repeat("This is additional content to make the comment longer so it will be split. ", 4) + "\n</details>",
+			maxSize:       200,
+			maxComments:   0,
+			command:       "plan",
+			expectedCount: 4,
+			expectedComments: []string{
+				"<details open><summary>Show Output</summary>\n\nSome details content here. This is additional content to make the comment longer so it will be </details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan -->split. This is additional content to make the comment longer so it will be split. This is additional content to make </details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan -->the comment longer so it will be split. This is additional content to make the comment longer so it will be split. \n</details>\n<!-- END -->",
+				"<details><summary>Show Output</summary>\n<!-- START plan --></details>",
+			},
+		},
+		{
+			name: "DetailsBlockWithAttributes - Splitting within details block with attributes -2",
+			comment: strings.ReplaceAll(`# Some header 1
+<details open><summary>Show Output 1a.</summary>
+$$$diff
+Code block within an OPEN details block 1.
+$$$
+</details>
+
+<details><summary>Show Output 1b.</summary>
+$$$diff
+Code block within a CLOSED details block.
+$$$
+</details>
+
+# Some header 2
+
+<details open><summary>Show Output 2a.</summary>
+$$$diff
+Code block within an OPEN details block 2.
+$$$
+</details>
+
+<details><summary>Show Output 2b.</summary>
+$$$diff
+Code block within a CLOSED details block 2.
+$$$
+</details>
+
+# Some header 3
+
+$some single line code block 1$
+$some single line code block 2$
+$some single line code block 3$
+</details>`, "$", "`"),
+			maxSize:       200,
+			maxComments:   0,
+			command:       "plan",
+			expectedCount: 5,
+			expectedComments: []string{
+				strings.ReplaceAll(`# Some header 1
+<details open><summary>Show Output 1a.</summary>
+$$$diff
+Code block within an OPEN details block 1.
+$$$
+</details>
+<!-- END -->`, "$", "`"),
+				strings.ReplaceAll(`<!-- START plan -->
+<details><summary>Show Output 1b.</summary>
+$$$diff
+Code block within a CLOSED details block.
+$$$
+</details>
+
+# Some header 2
+<!-- END -->`, "$", "`"),
+				strings.ReplaceAll(`<!-- START plan -->
+<details open><summary>Show Output 2a.</summary>
+$$$diff
+Code block within an OPEN details block 2.
+$$$
+</details>
+<!-- END -->`, "$", "`"),
+				strings.ReplaceAll(`<!-- START plan -->
+<details><summary>Show Output 2b.</summary>
+$$$diff
+Code block within a CLOSED details block 2.
+$$$
+</details>
+<!-- END -->`, "$", "`"),
+				strings.ReplaceAll(`<!-- START plan -->
+# Some header 3
+
+$some single line code block 1$
+$some single line code block 2$
+$some single line code block 3$
+</details>`, "$", "`"),
 			},
 		},
 	}
@@ -192,7 +331,12 @@ func TestSplitComment_DynamicSeparators(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			split := common.SplitComment(logger, tt.comment, tt.maxSize, tt.maxComments, tt.command)
 
-			Assert(t, len(split) == tt.expectedCount, "Expected %d comments, got %d", tt.expectedCount, len(split))
+			if len(split) != tt.expectedCount {
+				t.Errorf("Expected %d comments, got %d", tt.expectedCount, len(split))
+				for i, s := range split {
+					t.Logf("Got comment %d: %q", i, s)
+				}
+			}
 
 			// Compare each comment with expected output
 			for i, expected := range tt.expectedComments {
@@ -201,11 +345,9 @@ func TestSplitComment_DynamicSeparators(t *testing.T) {
 						"Comment %d mismatch:\nExpected: %s\nGot:      %s",
 						i, expected, split[i])
 				}
-				// Verify comment doesn't exceed maxSize
-				if len(split[i]) > tt.maxSize {
-					t.Errorf("Comment %d exceeds maxSize! Length: %d > %d", i, len(split[i]), tt.maxSize)
+				if i < len(split) && len(split[i]) > tt.maxSize {
+					t.Errorf("Comment %d exceeds maxSize: length %d > %d", i, len(split[i]), tt.maxSize)
 				}
-
 			}
 		})
 	}
