5533cbd0b56b6d856f62640afd9e3bdc5ca43dd0
Jhan Silva <jhancarlossilva.c@gmail.com>
2026-07-09 23:13:12 -0700

    fix: add parenthesis enclosed commit link and new test for working dir
    Signed-off-by: Jhan Silva <jhancarlossilva.c@gmail.com>

--- server/events/working_dir_locker_test.go
+++ server/events/working_dir_locker_test.go
@@ -46,7 +46,7 @@ func TestTryLockIncludesCommitMetadata(t *testing.T) {
 		want     string
 	}{
 		{name: "sha", metadata: events.WorkingDirLockMetadata{HeadCommit: sha}, want: "by \"plan\" for commit " + sha + "."},
-		{name: "link", metadata: events.WorkingDirLockMetadata{HeadCommit: sha, CommitURL: commitURL}, want: "by \"plan\" for commit [" + sha + "](" + commitURL + ")."},
+		{name: "url", metadata: events.WorkingDirLockMetadata{HeadCommit: sha, CommitURL: commitURL}, want: "by \"plan\" for commit " + sha + " (" + commitURL + ")."},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -66,7 +66,7 @@ func TestTryLockPullIncludesCommitMetadata(t *testing.T) {
 	_, err := locker.TryLockPull(repo, 1, command.Plan, events.WorkingDirLockMetadata{HeadCommit: sha, CommitURL: commitURL})
 	Ok(t, err)
 	_, err = locker.TryLockPull(repo, 1, command.Apply, events.WorkingDirLockMetadata{})
-	ErrContains(t, "by \"plan\" for commit ["+sha+"]("+commitURL+").", err)
+	ErrContains(t, "by \"plan\" for commit "+sha+" ("+commitURL+").", err)
 }
 
 func TestTryLockSameCommand(t *testing.T) {
